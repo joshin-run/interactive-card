973b1d15b805846c6e6a95ff505e8acd0e0dcb13
Josh Holladay <josh@jcreateit.com>
2018-09-21 16:31:48 -0600

    Feat - change button click color, change app name and icono in chrome tab, readme, add custom icons

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
 import CreditCard from './CreditCard'
 import TermLoan from './TermLoan'
+import creditCard from '../assets/creditCard2.png'
+import moneyBag from '../assets/moneyBag2.png'
 
 class Products extends Component {
   constructor(props) {
     super(props)
-    this.updateScreen = this.updateScreen.bind(this)
     this.state = {
-      screenIndex: 1
+      screenIndex: 1,
+      active: false
     }
+    this.updateScreen = this.updateScreen.bind(this),
+    this.setButtonColor = this.setButtonColor.bind(this),
+    this.buttonClick = this.buttonClick.bind(this)
+  }
+
+  setButtonColor() {
+    this.setState({active: !this.state.active})
   }
 
   updateScreen(newScreenIndex) {
     this.setState({
       screenIndex: newScreenIndex
     });
+  }
+
+  buttonClick(newScreenIndex) {
+    this.setButtonColor();
+    this.updateScreen(newScreenIndex)
   }
 
   render() {
@@ -30,8 +44,15 @@ class Products extends Component {
     return (
       <div className="products">
         <div className="button-wrapper">
-          <button className="nav-buttons credit-card-button" onClick={() => { this.updateScreen(1)}}>Credit Card</button>
-          <button className="nav-buttons term-loan-button" onClick={() => { this.updateScreen(2)}}>Term Loan</button>
+          <button id="credit-card-button" className={`nav-buttons ${this.state.screenIndex == 1 ? 'active' : ''}`} onClick={() => {
+            this.buttonClick(1);
+          }}>
+            <img className="cc-icon button-icons" src={creditCard} />
+            Credit Card
+          </button>
+          <button id="term-loan-button" className={`nav-buttons ${this.state.screenIndex == 2 ? 'active' : ''}`} onClick={() => { this.buttonClick(2)}}>
+            <img className="mb-icon button-icons" src={moneyBag} />
+          Term Loan</button>
         </div>
         <div className="screen-wrapper">
           {ActiveScreen}
