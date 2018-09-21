81defdd09908b50c0671523245816afacda31059
Josh Holladay <josh@jcreateit.com>
2018-09-21 13:23:40 -0600

    Fix - issue with .scss file not updateing for term-loan

--- src/components/TermLoan.js
+++ src/components/TermLoan.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import bizImg from '../assets/business.svg'
+import addIcon from '../assets/addIcon.svg'
 
 class TermLoan extends Component {
   render() {
     return (
       <div className="term-loan">
         <div className="add-wrapper">
-          <img className="biz-img" src={bizImg} />
+          <img className="biz-image" src={bizImg} />
+          <h1>Add your business</h1>
+          <p>For term loan options, add your business</p>
+          <img className="biz-image" src={addIcon} />
+          <p>Add</p>
         </div>
       </div>
     );
